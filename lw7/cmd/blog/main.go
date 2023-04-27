b5239f4e79ceb558d912d54d489d4577ad029d79
chkqq <124921297+chkqq@users.noreply.github.com>
2023-04-27 22:11:26 +0300

    f

--- lw7/cmd/blog/main.go
+++ lw7/cmd/blog/main.go
@@ -1,23 +1,15 @@
 package main
 
-// для чего имопрт?
-// для подключения пакетов логирования и пакета для http сервера
 import (
-	"database/sql" // Импортируем для возможности подключения к MySQL
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
 
-	_ "github.com/go-sql-driver/mysql"
+	_ "github.com/go-sql-driver/mysql" // Импортируем для возможности подключения к MySQL
+	"github.com/gorilla/mux"
 	"github.com/jmoiron/sqlx"
 )
-
-// на каком порту нельзя запустить сервер?
-// На зарезервированных например:
-// Порты от 0 до 1023: эти порты зарезервированы для системных служб и могут быть использованы только суперпользователем или системными процессами.
-// Порты 135-139 и 445: эти порты зарезервированы для службы NetBIOS, используемой в операционных системах Windows.
-// Порты 1433 и 1434: эти порты зарезервированы для Microsoft SQL Server.
-// Порты 80 и 443: эти порты зарезервированы для HTTP и HTTPS соответственно, и они используются для веб-серверов.
 
 const (
 	port         = ":3000"
@@ -33,17 +25,17 @@ func main() {
 
 	dbx := sqlx.NewDb(db, dbDriverName) // Расширяем стандартный клиент к базе
 
-	//createDatabase(dbx)
+	r := mux.NewRouter()
 
-	mux := http.NewServeMux()
-	mux.HandleFunc("/home", index(dbx))
-	mux.HandleFunc("/post", post)
+	r.HandleFunc("/home", index(dbx))
+	r.HandleFunc("/post/{postID}", post(dbx))
 
-	// Реализуем отдачу статики
-	mux.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
+	// // Реализуем отдачу статики
+	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
+	// mux.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
 
 	fmt.Println("Start server")
-	err = http.ListenAndServe(port, mux)
+	err = http.ListenAndServe(port, r)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -51,5 +43,5 @@ func main() {
 
 func openDB() (*sql.DB, error) {
 	// Здесь прописываем соединение к базе данных
-	return sql.Open(dbDriverName, "root:Q19euplzo12345)@tcp(localhost:3306)/blog?charset=utf8mb4&collation=utf8mb4_unicode_ci&parseTime=true")
+	return sql.Open(dbDriverName, "root:rQ19euplzo12345)@tcp(localhost:3306)/blog?charset=utf8mb4&collation=utf8mb4_unicode_ci&parseTime=true")
 }
